c40adcdb6a34fcc9752ed6beefefd82adc90984c
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-11-29 10:24:30 +0100

    merge master y login_register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Townout</title>
 
     <title>Townout</title>
 
@@ -133,10 +134,10 @@
                     <button type="submit" class="btn btn-primary">Registrar</button>
                 </div>
             </div>
+          </div>
         </div>
     </div>
-
-    <!-- Modal Login-->
+<!-- Modal Login-->
     <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="registerModalTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
@@ -203,102 +204,132 @@
             <h1 class="display-4 font-weight-bold">Una nueva forma de descubrir el mundo</h1>
             <h3 class="display-4">Con TownOut el turismo pasa a un nuevo nivel. Explora los rincones más emblemáticos de todo el mundo mientras superas acertijos y pruebas</h3>
         </div>
-        <div id="logo">
-            <img src="{{asset('/assets/img/logo.svg')}}" class="scaledsvg">
-        </div>
-        <div id="mobile">
-            <img src="{{asset('/assets/img/mobile.png')}}">
-        </div>
-      
-        <!-- PLACEMARKS PARA ANIMACIÓN-->
-        <svg id="pm0" class="placemarks" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 255.856 255.856" xml:space="preserve">
-            <g>
-                <path style="fill:#bd2830" d="M127.928,38.8c-30.75,0-55.768,25.017-55.768,55.767s25.018,55.767,55.768,55.767
+    </div>
+
+        <header id="header">
+            <div id="header-texto">
+                <h1 class="display-4 font-weight-bold">Una nueva forma de descubrir el mundo</h1>
+                <h3 class="display-4">Con TownOut el turismo pasa a un nuevo nivel. Explora los rincones más emblemáticos de todo el mundo mientras superas acertijos y pruebas</h3>
+            </div>
+            <div id="logo">
+                <img src="{{asset('/assets/img/logo.svg')}}" class="scaledsvg">
+            </div>
+            <div id="mobile">
+                <img src="{{asset('/assets/img/mobile.png')}}">
+            </div>
+            <!-- PLACEMARKS PARA ANIMACIÓN-->
+            <svg id="pm0" class="placemarks" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 255.856 255.856" xml:space="preserve">
+                <g>
+                    <path style="fill:#bd2830" d="M127.928,38.8c-30.75,0-55.768,25.017-55.768,55.767s25.018,55.767,55.768,55.767
                         s55.768-25.017,55.768-55.767S158.678,38.8,127.928,38.8z M127.928,135.333c-22.479,0-40.768-18.288-40.768-40.767
-                        S105.449,53.8,127.928,53.8s40.768,18.288,40.768,40.767S150.408,135.333,127.928,135.333z" />
-                <path style="fill:#bd2830" d="M127.928,0C75.784,0,33.362,42.422,33.362,94.566c0,30.072,25.22,74.875,40.253,98.904
+                        S105.449,53.8,127.928,53.8s40.768,18.288,40.768,40.767S150.408,135.333,127.928,135.333z"/>
+                    <path style="fill:#bd2830" d="M127.928,0C75.784,0,33.362,42.422,33.362,94.566c0,30.072,25.22,74.875,40.253,98.904
                         c9.891,15.809,20.52,30.855,29.928,42.365c15.101,18.474,20.506,20.02,24.386,20.02c3.938,0,9.041-1.547,24.095-20.031
                         c9.429-11.579,20.063-26.616,29.944-42.342c15.136-24.088,40.527-68.971,40.527-98.917C222.495,42.422,180.073,0,127.928,0z
                          M171.569,181.803c-19.396,31.483-37.203,52.757-43.73,58.188c-6.561-5.264-24.079-26.032-43.746-58.089
                         c-22.707-37.015-35.73-68.848-35.73-87.336C48.362,50.693,84.055,15,127.928,15c43.873,0,79.566,35.693,79.566,79.566
-                        C207.495,112.948,194.4,144.744,171.569,181.803z" />
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-            <g>
-            </g>
-        </svg>
-        <img class="placemarks" id="pm1" src="{{asset('/assets/img/icons/placemark.svg')}}">
-        <img class="placemarks" id="pm2" src="{{asset('/assets/img/icons/placemark.svg')}}">
-        <img class="placemarks" id="pm3" src="{{asset('/assets/img/icons/placemark.svg')}}">
+                        C207.495,112.948,194.4,144.744,171.569,181.803z"/>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                <g>
+                </g>
+                </svg>
+            <img class="placemarks" id="pm1" src="{{asset('/assets/img/icons/placemark.svg')}}">
+            <img class="placemarks" id="pm2" src="{{asset('/assets/img/icons/placemark.svg')}}">
+            <img class="placemarks" id="pm3" src="{{asset('/assets/img/icons/placemark.svg')}}">
 
 
 
-        <a href="#s1" class="same-page-nav" id="arrow_down"><img src="{{asset('/assets/img/icons/arrow_down.svg')}}"></a>
-    </header>
-    <section id="s1">
-        <h1>Explora tu ciudad</h1>
-        <a href="#contacto" class="same-page-nav" id="contacto-link">CONTACTO</a>
-    </section>
-    <section id="s2">
-        <div>
-            <h2>Crea tu propia experiencia</h2>
-            <img src="{{asset('/assets/img/qa.svg')}}">
-        </div>
-        <div>
-            <h2>o vive una ya diseñada</h2>
-            <img src="{{asset('/assets/img/explorer.svg')}}">
-        </div>
-    </section>
-    <section id="contacto">
-        <h2>¡Contacta con nosotros!</h2>
-        <form action="{{route('contact-message')}}" method="post" id="contacto-form">
-            @csrf
-            <div id="inputs">
-                <input type="text" name="nombre" placeholder="Nombre">
-                <span class="error" data-for="nombre"></span>
-                <input type="email" name="email" placeholder="Correo electrónico">
-                <span class="error" data-for="email"></span>
-                <textarea name="mensaje" placeholder="Mensaje"></textarea>
-                <span class="error" data-for="mensaje"></span>
+            <a href="#s1" class="same-page-nav" id="arrow_down"><img src="{{asset('/assets/img/icons/arrow_down.svg')}}"></a>
+        </header>
+        <section id="s1">
+            <h1 class="display-4">La forma más fácil de conocer nuevos lugares</h1>
+            <a href="#contacto" class="same-page-nav" id="contacto-link">CONTACTO</a>
+        </section>
+        <section id="s2">
+            <div>
+                <h2>Crea tu propia experiencia</h2>
+                <img src="{{asset('/assets/img/qa.svg')}}">
             </div>
-            <button type="button" id="send"><img src="{{asset('/assets/img/icons/send.svg')}}"></button>
-        </form>
-    </section>
-    <footer>
-        Koldo Intxausti &copy, 2019
-        <div>
-            <img src="{{asset('/assets/img/icons/instagram.svg')}}">
-            <img src="{{asset('/assets/img/icons/twitter.svg')}}">
-            <img src="{{asset('/assets/img/icons/facebook.svg')}}">
-        </div>
-    </footer>
-</body>
-
+            <div>
+                <h2>o vive una ya diseñada</h2>
+                <img src="{{asset('/assets/img/explorer.svg')}}">
+            </div>
+        </section>
+        <section id="s3">
+            <div class="card-deck p-4 col-12">
+                <div class="card col-lg-4 col-md-6 col-sm-12">
+                    <img src="{{asset('assets/img/brujula.png')}}" class="card-img-top cardImg align-self-start mt-1 p-4" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold text-uppercase">Inicia TownOut</h5>
+                        <p class="card-text">Accede a la página web desde cualquier dispositivo con conexión a internet</p>
+                    </div>
+                </div>
+                <div class="card col-lg-4 col-md-6 col-sm-12">
+                    <img src="{{asset('assets/img/interrogacion.svg')}}" class="card-img-top cardImg align-self-start mt-1 p-4" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold text-uppercase">Escoge una experiencia</h5>
+                        <p class="card-text">Escoge la experiencia que quieras vivir del la selección. Usuarios como guías turísticos profesionales pueden crearlas.</p>
+                    </div>
+                </div>
+                <div class="card col-lg-4 col-md-6 col-sm-12">
+                    <img src="{{asset('assets/img/pointer.png')}}" class="card-img-top cardImg align-self-start mt-1 p-4" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold text-uppercase">Empieza a explorar</h5>
+                        <p class="card-text">Sigue las pistas, resuelve los acertijos y supera el reto</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contacto">
+            <h2>¡Contacta con nosotros!</h2>
+            <form action="{{route('contact-message')}}" method="post" id="contacto-form">
+                @csrf
+                <div id="inputs">
+                    <input type="text" name="nombre" placeholder="Nombre">
+                    <span class="error" data-for="nombre"></span>
+                    <input type="email" name="email" placeholder="Correo electrónico">
+                    <span class="error" data-for="email"></span>
+                    <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                    <span class="error" data-for="mensaje"></span>
+                </div>
+                <button type="button" id="send"><img src="{{asset('/assets/img/icons/send.svg')}}"></button>
+            </form>
+        </section>
+        <footer>
+            Xabier Artola & Koldo Intxausti & Nerea Labandera &copy<br>2019
+            <div>
+                <img src="{{asset('/assets/img/icons/instagram.svg')}}">
+                <img src="{{asset('/assets/img/icons/twitter.svg')}}">
+                <img src="{{asset('/assets/img/icons/facebook.svg')}}">
+            </div>
+        </footer>
+    </body>
 </html>
