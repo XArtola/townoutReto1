a3f4e5adac960b131f028856574d8cca4c017b2b
koldointxausti <kinchaustide18dw@ikzubirimanteo.com>
2019-12-05 10:08:56 +0100

    más correcciones de validaciones de formularios

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,38 +13,49 @@ $(document).ready(function(){
 		let email = $('#inputs input[name="email"]').val();
 		let mensaje = $('#inputs textarea[name="mensaje"]').val();
 
-		if(!nombre)
-			$('.error[data-for="nombre"]').text('Este campo es obligatorio');
-		if(!apellido)
-			$('.error[data-for="apellido"]').text('Este campo es obligatorio');
-		if(!email)
-			$('.error[data-for="email"]').text('Este campo es obligatorio');
-		if(!mensaje)
-			$('.error[data-for="mensaje"]').text('Este campo es obligatorio');
+		let correct = true;
 
-		if(nombre && apellido && email && mensaje){
-			let correct = true;
+		if(!nombre){
+			correct = false;
+			$('.error[data-for="nombre"]').text('Este campo es obligatorio');
+		}else{
 			if(!nombre.match(/^[a-zñÑáéíóúÁÉÍÓÚ\s]+$/i)){
 				correct = false;
 				$('.error[data-for="nombre"]').text('No se admiten números o símbolos.');
 			}else $('.error[data-for="nombre"]').empty();
+		}
 
+		if(!apellido){
+			correct = false;
+			$('.error[data-for="apellido"]').text('Este campo es obligatorio');
+		}else{
 			if(!apellido.match(/^[a-zñÑáéíóúÁÉÍÓÚ\s]+$/i)){
 				correct = false;
 				$('.error[data-for="apellido"]').text('No se admiten números o símbolos.');
 			}else $('.error[data-for="apellido"]').empty();
-			
+		}
+
+		if(!email){
+			correct = false;
+			$('.error[data-for="email"]').text('Este campo es obligatorio');
+		}else{
 			if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
 				correct = false;
 				$('.error[data-for="email"]').text('Formato inválido para un correo electrónico.');
 			}else $('.error[data-for="email"]').empty();
+		}
 
+		if(!mensaje){
+			correct = false;
+			$('.error[data-for="mensaje"]').text('Este campo es obligatorio');
+		}else{
 			if(!mensaje.match(/^[a-z0-9ñÑáéíóúÁÉÍÓÚ\s,.:;-]+$/i)){
 				correct = false;
 				$('.error[data-for="mensaje"]').text('Formato de texto no aceptado.');
 			}
-			if(correct) $('#contacto-form').submit();
 		}
+		
+		if(correct) $('#contacto-form').submit();
 	});
 
 	//validación de registro de usuario
@@ -56,58 +67,69 @@ $(document).ready(function(){
 		let password = $('.register input[name="password"]').val();
 		let password_confirmation = $('.register input[name="password_confirmation"]').val();
 
-		//let error = '@lang(\'validation.filled\')';
+		let correct = true;
 
-		if(!username)
+		if(!username){
+			correct = false
 			$('.error[data-for="regis_username"]').text('Debes introducir un nombre de usuario');
-		if(!name)
-			$('.error[data-for="regis_name"]').text('Debes introducir un nombre');
-		if(!surname)
-			$('.error[data-for="regis_surname"]').text('Debes introducir un apellido');
-		if(!email)
-			$('.error[data-for="regis_email"]').text('Debes introducir un email');
-		if(!password)
-			$('.error[data-for="regis_password"]').text('Debes introducir una contraseña');
-		if(!password_confirmation)
-			$('.error[data-for="regis_confirmpassword"]').text('Debes repetir la contraseña');
-
-		if(username && name && surname && email && password && password_confirmation){
-			let correct = true;
-			
+		}else {
 			if (!username.match(/^[a-zñÑáéíóúÁÉÍÓÚ0-9]+$/i)) {
 				correct = false;
 				$('.error[data-for="regis_username"]').text('El nombre de usuario solo puede contener letras y numeros sin espacios');
 			}else $('.error[data-for="regis_username"]').empty();
+		}
 
+		if(!name){
+			correct = false
+			$('.error[data-for="regis_name"]').text('Debes introducir un nombre');
+		}else{
 			if(!name.match(/^[a-zñÑáéíóúÁÉÍÓÚ\s]+$/i)){
 				correct = false;
 				$('.error[data-for="regis_name"]').text('No se admiten números o símbolos.');
 			}else $('.error[data-for="regis_name"]').empty();
+		}
 
+		if(!surname){
+			correct = false
+			$('.error[data-for="regis_surname"]').text('Debes introducir un apellido');
+		}else{
 			if(!surname.match(/^[a-zñÑáéíóúÁÉÍÓÚ\s]+$/i)){
 				correct = false;
 				$('.error[data-for="regis_surname"]').text('No se admiten números o símbolos.');
 			}else $('.error[data-for="regis_surname"]').empty();
-			
+		}
+
+		if(!email){
+			correct = false
+			$('.error[data-for="regis_email"]').text('Debes introducir un email');
+		}else{
 			if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
 				correct = false;
 				$('.error[data-for="regis_email"]').text('Formato inválido para un correo electrónico.');
 			}else $('.error[data-for="regis_email"]').empty();
+		}
 
+		if(!password){
+			correct = false
+			$('.error[data-for="regis_password"]').text('Debes introducir una contraseña');
+		}else{
 			if(!password.match(/^[a-z0-9]{8,}$/)){
 				correct = false;
 				$('.error[data-for="regis_password"]').text('La longitud mínima son 8 carácteres y solo puede contener letras y números');
 			}else $('.error[data-for="regis_password"]').empty();
-			
+		}
+
+		if(!password_confirmation){
+			correct = false
+			$('.error[data-for="regis_confirmpassword"]').text('Debes repetir la contraseña');
+		}else{
 			if(!password_confirmation.match(password)){
 				correct = false;
 				$('.error[data-for="regis_confirmpassword"]').text('Repita contraseña');
 			}else $('.error[data-for="regis_confirmpassword"]').empty();
-
-
-			if(correct) $('#register_form').submit();
 		}
 
+		if(correct) $('#register_form').submit();
 
 	});
 
@@ -120,31 +142,29 @@ $(document).ready(function(){
 		let email = $('.login input[name="email"]').val();
 		let password = $('.login input[name="password"]').val();
 
+		let correct = true;
 
-		if(!email)
+		if(!email){
+			correct = false;
 			$('.error[data-for="login_email"]').text('Introduce email');
-		if(!password)
-			$('.error[data-for="login_password"]').text('Introduce la contraseña');
-
-
-		if(email && password){
-			let correct = true;
-			
+		}else{
 			if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
 				correct = false;
-				$('.error[data-for="login_mail"]').text('Formato de correo electrónico inválido');
+				$('.error[data-for="login_email"]').text('Formato de correo electrónico inválido');
 			}else $('.error[data-for="regis_username"]').empty();
-
-			if(!password.match(/^[a-z0-9]{8,}$/)){
-				correct = false;
-				$('.error[data-for="login_password"]').text('Contraseña no válida');
-			}else $('.error[data-for="login_password"]').empty();
-
-
-
-			if(correct) $('#login_form').submit();
 		}
 
+		if(!password){
+			correct = false;
+			$('.error[data-for="login_password"]').text('Introduce la contraseña');
+		}else{
+			if(!password.match(/^[^<>()\[\]\\.,;:\s@"]*$/)){
+				correct = false;
+				$('.error[data-for="login_password"]').text('La contraseña tiene carácteres no válidos');
+			}else $('.error[data-for="login_password"]').empty();
+		}
+
+		if(correct) $('#login_form').submit();
 
 	});
 
@@ -154,36 +174,39 @@ $(document).ready(function(){
 		let password = $('.reset input[name="password"]').val();
 		let confpassword = $('.reset input[name="password_confirmation"]').val();
 
+		let correct = true;
 
-
-		if(!email)
+		if(!email){
+			correct = false;
 			$('.error[data-for="reset_email"]').text('Introduce email');
-		if(!password)
-			$('.error[data-for="reset_password"]').text('Introduce la contraseña');
-		if(!confpassword)
-			$('.error[data-for="reset_password_confirmation"]').text('Repite la contraseña');
-
-
-		if(email && password && confpassword){
-			let correct = true;
-			
+		}else{
 			if(!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
 				correct = false;
 				$('.error[data-for="reset_email"]').text('Formato de correo electrónico inválido');
 			}else $('.error[data-for="reset_password"]').empty();
+		}
 
+		if(!password){
+			correct = false;
+			$('.error[data-for="reset_password"]').text('Introduce la contraseña');
+		}else{
 			if(!password.match(/^[a-z0-9]{8,}$/)){
 				correct = false;
 				$('.error[data-for="reset_password"]').text('Contraseña no válida');
-			}else $('.error[data-for="reset_password"]').empty();
+			}else $('.error[data-for="reset_password"]').empty();							
+		}
 
+		if(!confpassword){
+			correct = false;
+			$('.error[data-for="reset_password_confirmation"]').text('Repite la contraseña');
+		}else{
 			if(!confpassword.match(password)){
 				correct = false;
 				$('.error[data-for="reset_password_confirmation"]').text('No cincide con la contraseña');
 			}else $('.error[data-for="reset_password_confirmation"]').empty();
-
-			if(correct) $('#reset_send').submit();
 		}
+
+		if(correct) $('#reset_send').submit();
 
 
 	});
