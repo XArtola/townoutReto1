ef824db805e925dd3e47e719f5db14df67453cf2
xabi <xartolaza18dw@ikzubirimanteo.com>
2020-01-07 19:28:37 +0100

    formulario terminado

--- resources/views/circuits/circuits.blade.php
+++ resources/views/circuits/circuits.blade.php
@@ -14,8 +14,8 @@
 		<option value="img">Img</option>
 	</select>
 
-	<form id="textForm" method="POST" action="{{route}}">
-		@crsf
+	<form method="POST" action="{{'stages'}}">
+		@csrf
 		<label>Question text</label>
 		<input type="text" name="question_text">
 		<label>Question image</label>
@@ -24,28 +24,30 @@
 		<input type="hidden" name="lng">
 		<input type="hidden" name="order">
 		<input type="hidden" name="circuit_id">
-		<input type="text" name="answer">
-		<input type="submit">
-	</form>
-	<form id="quizForm">
-		<label>Correct answer</label>
-		<input type="text" name="correct_ans">
-		<label>Possible answer 1</label>
-		<input type="text" name="possible_ans1">
-		<label>Possible answer 2</label>
-		<input type="text" name="possible_ans2">
-		<label>Possible answer 3</label>
-		<input type="text" name="possible_ans3">
+		<div id="textForm" class="stageForm">
+			<label>Answer<label>
+			<input type="text" name="answer">
+		</div>
+		<div id="quizForm" class="stageForm">
+			<label>Correct answer</label>
+			<input type="text" name="correct_ans">
+			<label>Possible answer 1</label>
+			<input type="text" name="possible_ans1">
+			<label>Possible answer 2</label>
+			<input type="text" name="possible_ans2">
+			<label>Possible answer 3</label>
+			<input type="text" name="possible_ans3">
+			<input type="submit">
+		</div>
 		<input type="submit">
 	</form>
 
 	<script type="text/javascript">
 		$(function() {
-			$('form').hide();
-			$('#textForm').show();
-			
+			$('#quizForm').hide();
+
 			$('#selector').change(function(event) {
-				$('form').hide();
+				$('.stageForm').hide();
 				$('#' + $('#selector').val() + "Form").show();
 			});
 
