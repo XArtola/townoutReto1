895677fd892f7b7f5b92fec227d993961c892a2b
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2020-01-08 11:00:55 +0100

    user_home dinamico

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.main')
 @section('title','Index')
 @section('content')
-HOME de user no admin (no sé que hay que añadir aqui)
+
+@foreach($circuits as $circuit)
+<div>
+	<h2>Name: $circuit->name</h2>
+	<p>Creator: $circuit->user_id</p>
+	<p>Location: $circuit->location</p>
+	<p>Estimated time: &circuit->duration</p>
+	<p>$circuit->difficulty</p>
+</div>
+@endforeach
 @endsection
 
 @section('js')
