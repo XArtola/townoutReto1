7622f57d058830a64e90977ec63ac49ffc9846c7
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2020-01-08 13:17:13 +0100

    listado de circuitos disponibles

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,22 +1,38 @@
 @extends('layouts.main')
 @section('title','Index')
 @section('content')
-
-@foreach($circuits as $circuit)
-<div>
-	<h2>Name: $circuit->name</h2>
-	<p>Creator: $circuit->user_id</p>
-	<p>Location: $circuit->location</p>
-	<p>Estimated time: &circuit->duration</p>
-	<p>$circuit->difficulty</p>
+<div id="all_circuits">
+	<h1>Circuitos disponibles</h1>
+	<div class="row">
+		@foreach($circuits as $circuit)
+		<div class="col">
+			<h2>Name: {{$circuit->name}}</h2>
+			<p>Creator: {{$circuit->user->name}}</p>
+			<p>Location: {{$circuit->city}}</p>
+			<p>Estimated time: {{$circuit->duration}}</p>
+			<p>Difficulty:{{$circuit->dificulty}}</p>
+		</div>
+		@endforeach
+	</div>
+	<div>
+		<button>Unirse a un circuito</button>
+		<a href=""><button>New Circuit</button></a>
+	</div>
+	
 </div>
-@endforeach
+
+<!-- Futura implementación -->
+<!--
+<div>
+	<h1>My Circuits</h1>
+</div>
+-->
 @endsection
 
 @section('js')
-    $(document).ready(function(){
-        $('table form input[type="button"]').click(function(){
-            $(this).parent('form').submit();
-        });
-    });
+$(document).ready(function(){
+$('table form input[type="button"]').click(function(){
+$(this).parent('form').submit();
+});
+});
 @endsection
