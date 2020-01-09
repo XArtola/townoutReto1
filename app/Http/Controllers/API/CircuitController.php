22d187a6df9c24e0370af80b0a68cb5d57ef64b4
XArtola <xartolaza18dw@ikzubirimanteo.com>
2020-01-09 13:57:41 +0100

    resources

--- app/Http/Controllers/API/CircuitController.php
+++ app/Http/Controllers/API/CircuitController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Circuit;
 
 class CircuitController extends Controller
 {
-    //
+ 
+      /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+
+        $circuit = Location::all();
+        return $this->sendResponse(LocationResource::collection($products), 'Locations retrieved succesfully.');
+        $circuit = Circuit::find($id);
+
+    }
+
+
 }
