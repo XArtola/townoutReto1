71778bcf03eb3ddf38ac109c8897f83f34d49228
koldointxausti <koldointxaustii@gmail.com>
2020-01-10 09:28:21 +0100

    map centering. ask only first time for location (not finished)

--- resources/views/circuits/map.blade.php
+++ resources/views/circuits/map.blade.php
@@ -20,27 +20,69 @@
 
 @section('js')
 	$(function(){
-		var mymap = L.map('mapid').setView([51.505, -0.09], 5);
-		//mymap = L.map('mapid', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16});
-		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-			maxZoom: 18,
-			id: 'mapbox/streets-v11',
-			accessToken: 'pk.eyJ1IjoieGFydG9sYSIsImEiOiJjazQ4bno1bTEwbjI0M2twYThnNDJvcTQ4In0.MVU78eV__a2jJE2VkNTCfQ'
-		}).addTo(mymap);
 
-		let markers = $.ajax({
-			url: "http://localhost:8000/api/markers/{{$circuit_id}}/",
-			method: "GET",
-			success: function(data){
-				for(let i = 0; i < data.data.length; i++){
-					let marker = L.marker(data.data[i]).addTo(mymap);
-				}
-			},
-			error: function(error){
-				console.error(error.status)
+		var mymap = L.map('mapid');
+		var crd = null;
+
+		var options = {
+			enableHighAccuracy: true,
+			timeout: 5000,
+			maximumAge: 0
+		};
+
+		function success(pos) {
+			crd = pos.coords;
+			let coor = [crd.latitude,crd.longitude];
+			console.log(coor)
+			sessionStorage.setItem('current_position',coor);
+			mymap.setView(coor, 13);
+		};
+
+		function error(err) {
+			console.warn('ERROR(' + err.code + '): ' + err.message);
+			// pone una ubicación por defecto
+			crd = [51.505, -0.09];
+			sessionStorage.setItem('current_position',crd);
+			mymap.setView(crd, 13);
+		};
+		
+		// si no hay un current position guardado en el sessionStorage pregunta si queremos usar la ubicación. Si acepta carga el mapa en función a ella y si no carga una ubicación por defecto.
+		if(!sessionStorage.getItem('current_position'))
+			navigator.geolocation.getCurrentPosition(success, error, options);
+
+		renderMap();
+
+		// carga el mapa
+		function renderMap(){
+			if(sessionStorage.getItem('current_position')){
+				let coors = sessionStorage.getItem('current_position').split(',');
+				let coordinates = [parseFloat(coors[0]), parseFloat(coors[1])];
+
+				console.log(L.titleLayer)
+				L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+					attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+					maxZoom: 18,
+					id: 'mapbox/streets-v11',
+					accessToken: 'pk.eyJ1IjoieGFydG9sYSIsImEiOiJjazQ4bno1bTEwbjI0M2twYThnNDJvcTQ4In0.MVU78eV__a2jJE2VkNTCfQ'
+				}).addTo(mymap);
+
+				let markers = $.ajax({
+					url: "http://localhost:8000/api/markers/{{$circuit_id}}/",
+					method: "GET",
+					success: function(data){
+						for(let i = 0; i < data.data.length; i++){
+							let marker = L.marker(data.data[i]).addTo(mymap);
+						}
+					},
+					error: function(error){
+						console.error(error.status)
+					}
+				});
+			}else{
+				setTimeout(renderMap,2000);
 			}
-		});
+		}
+
 		let marker = null;
 		function onMapClick(e) {
 			//alert("You clicked the map at " + e.latlng);
