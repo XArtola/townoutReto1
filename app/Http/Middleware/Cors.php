41be4b360f2ae9166a0fdc5addd1f2cc4abd29c1
xabi <xartolaza18dw@ikzubirimanteo.com>
2020-01-10 19:13:51 +0100

    Arreglo de middleware cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,21 +9,12 @@ class Cors
     /**
      * Handle an incoming request.
      *
-<<<<<<< HEAD
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-=======
-     * @param \Illuminate\Http\Request $request
-     * @param \Closure $next
->>>>>>> feature/mapaJuego
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-<<<<<<< HEAD
-        return $next($request)->header("Access-Control-Allow-Origin","http://localhost")-> header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
-=======
         return $next($request)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
->>>>>>> feature/mapaJuego
     }
 }
