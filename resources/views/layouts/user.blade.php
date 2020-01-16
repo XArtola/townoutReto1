d06bc858d2ed87e0dc786aa7e6b9c458b3b881f5
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2020-01-16 11:38:31 +0100

    pequeños errores

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -69,8 +69,8 @@
             <img id="avatarImg" class="rounded-circle" src="{{Auth::user()->avatar ? route('storage','avatars/'.Auth::user()->avatar) : asset('/assets/img/logoPNG.png')}}"><img>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{route('user.show',['username'=>Auth::user()->username])}}">@lang('profile')</a>
-            <a class="dropdown-item" href="{{route('games.historic')}}">@lang('historic')</a>
+            <a class="dropdown-item" href="{{route('user.show',['username'=>Auth::user()->username])}}">@lang('user.profile')</a>
+            <a class="dropdown-item" href="{{route('games.historic')}}">@lang('games.historic')</a>
             <div class="dropdown-divider"></div>
 
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
