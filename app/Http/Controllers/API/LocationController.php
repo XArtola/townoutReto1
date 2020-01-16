bd49ff1dadc1fac4e4c8554c1ae51334c03e2e83
koldointxausti <kinchaustide18dw@ikzubirimanteo.com>
2020-01-16 21:46:09 +0100

    prueba10 caretaker monitoring

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -147,6 +147,6 @@ class LocationController extends BaseController
 
     public function lastLocation($id){
         $location = Location::where('game_id',$id)->latest()->first();
-        return $this->sendResponse(['location'=>$location]);
+        return $this->sendResponse(new LocationResource($location));
     }
 }
