5c923b57e024ab235961104330eb20db6e4ed2f8
XArtola <xartolaza18dw@ikzubirimanteo.com>
2020-01-23 09:39:21 +0100

    creación de rama

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -5,7 +5,7 @@
     <title>Prueba mapas</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
-    <link rel="stylesheet" type="text/css" href="{{secure_asset('/assets/css/game.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/assets/css/game.css',\App::environment() == 'production')}}">
     <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <style>
@@ -16,8 +16,8 @@
     </style>
     <script>
         //Para coger imgs desde JS
-        var base_url = "{{secure_asset('/')}}";
-        console.log(base_url);
+        var base_url = "{{asset('/',\App::environment() == 'production')}}";
+        //console.log(base_url);
     </script>
 </head>
 
@@ -360,7 +360,6 @@
 
                     //Evento onlocationfound (cada vez que la posición se actualice)
                     mymap.on('locationfound', function(data) {
-
                         if (firstLocation) {
                             latlng = [data.latitude, data.longitude];
                             firstLocation = false;
