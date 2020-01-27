9eb46b07329d6d77db1f0a11db3df9cf4d547517
koldointxausti <koldointxaustii@gmail.com>
2020-01-27 13:12:30 +0100

    mejoras en order

--- resources/views/circuit/order.blade.php
+++ resources/views/circuit/order.blade.php
@@ -3,25 +3,29 @@
 <script src="{{asset('/assets/js/circuits.js',\App::environment() == 'production')}}"></script>
 @endsection
 @section('content')
-	@foreach($circuit->stages as $stage)
-		<div class="stage">
-			<h2 style="font-size: 24px;">{{$stage->question_text}}</h2>
-			<div class="order-buttons" data-stageid="{{$stage->id}}" data-order="{{$stage->order}}">
-				<img src="{{asset('/assets/img/icons/arrow_down.svg',\App::environment() == 'production')}}" alt="arrow_up" class="arrow_up">
-				<img src="{{asset('/assets/img/icons/arrow_down.svg',\App::environment() == 'production')}}" alt="arrow_down" style="transform: rotate(180deg);" class="arrow_down">
+	<div id="stages">
+		@foreach($circuit->stages as $stage)
+			<div class="stage" data-order="{{$stage->order}}">
+				<h2 style="font-size: 24px;">{{$stage->question_text}}</h2>
+				<div class="order-buttons" data-stageid="{{$stage->id}}" >
+					<img src="{{asset('/assets/img/icons/arrow_down.svg',\App::environment() == 'production')}}" alt="arrow_down" class="arrow_down">
+					<img src="{{asset('/assets/img/icons/arrow_down.svg',\App::environment() == 'production')}}" alt="arrow_up" style="transform: rotate(180deg);" class="arrow_up">
+				</div>
 			</div>
-		</div>
-	@endforeach
-
+		@endforeach
+	</div>
 	<script>
 		$(document).ready(function(){
 
 			$('.arrow_up').click(function(){
-				if($(this).parent().attr('data-order') !== 0){
+				let order = $(this).parent().parent().data('order');
+				if(order > 1){
+					$('.stage[data-order="'+ (order - 1) +'"]').attr('data-order', order)
+					$(this).parent().parent().attr('data-order',(order - 1))
 
-					let order =$(this).parent().attr('data-order');
-					$('.order-buttons[data-order="'+ (order - 1) +'"]').attr('data-order', order)
-					$(this).parent().attr('data-order', (order - 1));
+					$('#stages').append($('#stages .stage').sort(function(a,b){
+					   return a.getAttribute('data-order')-b.getAttribute('data-order');
+					}));
 
 				}
 			});
