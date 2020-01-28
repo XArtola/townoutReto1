8edeac0e61ebdd333dc14087debb73a44c947b0e
koldointxausti <koldointxaustii@gmail.com>
2020-01-28 11:19:40 +0100

    cambio en API circuit prueba10

--- routes/web.php
+++ routes/web.php
@@ -122,7 +122,7 @@
 
             Route::get('/games/{id}/wait', 'GameController@wait')->name('games.wait');
             Route::get('/games/{id}/startCaretaker', 'GameController@startCaretaker')->name('games.startCaretaker');
-            Route::get('/games/{circuit}/monitor/{game_ids?}','GameController@monitor')->name('games.monitor');
+            Route::get('/games/{circuit}/monitor/{game_ids}','GameController@monitor')->name('games.monitor');
             Route::put('/games/{circuit}/endCaretaker', 'GameController@endCaretaker')->name('games.endCaretaker');
 
             Route::get('/games/{id}/destroy', 'GameController@destroy')->name('games.destroy');
