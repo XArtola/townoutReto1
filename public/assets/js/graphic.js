762da14f313469ff5d071e9c64c6938e3168997b
nlabandera <nlabanderafe18dw@ikzubirimanteo.com>
2020-01-28 11:12:30 +0100

    grafica played games funcional

--- public/assets/js/graphic.js
+++ public/assets/js/graphic.js
@@ -1,52 +1,91 @@
-google.charts.load('current', {'packages':['line', 'corechart']});
-	google.charts.setOnLoadCallback(drawChart);
+var request = new XMLHttpRequest();
+  var dates = [];
+  var cont = [];
+  var d;
+  var fecha;
+  
 
-	function drawChart() {
+  $.get('http://127.0.0.1:8000/api/games', function(games, statusTxt){
+
+    if(statusTxt =="success"){
+      console.log('success');
+      for(var game in games){
+           
+            d = new Date(games[game]['finish_date']);
+            fecha = d.getFullYear()+'-'+'0'+(d.getMonth()+1)+'-'+d.getDate();
+
+            //Si la fecha obtenida NO coincide con ninguna registrada en el array
+            if(!dates.includes(fecha)){
+              //Inserta la fecha en el array
+              dates.push(fecha);
+              //Inserta 1 en el array contador 
+              cont.push(1);
+
+            //Si la fecha obtenida SI existe en el array 
+            }else{
+              var c = cont.slice(-1).pop();
+              cont.pop();
+              cont.push(c+1);
+            
+            }
+  
+      }
+
+      console.log(dates);
+      console.log(cont);
+
+    }else
+        console.log('error');
+
+
+  });
+
+
+  google.charts.load('current', {'packages':['line', 'corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+
+  function drawChart() {
 
       //var button = document.getElementById('change-chart');
       var chartDiv = document.getElementById('chart_div');
 
       var data = new google.visualization.DataTable();
-      data.addColumn('date', 'Fecha');
-      data.addColumn('number', "Average Temperature");
+      data.addColumn('date', 'Date');
+      data.addColumn('number', "Played Games");
       
-
-      data.addRows([
-      	[new Date(2014, 0),   1],
-      	[new Date(2015, 0),   8],
-      	[new Date(2016, 0),   5],
-      	[new Date(2017, 0),   5],
-      	[new Date(2018, 0),   5]
-
-      	]);
+      for(var j = 0 ; j<dates.length; j++){
+        data.addRows([
+          [new Date(dates[j]), cont[j]],
+          ]);
+      }
 
       var materialOptions = {
-      	chart: {
-      		title: 'Gráfica de prueba'
-      	},
-      	width: 900,
-      	height: 500,
-      	series: {
+        chart: {
+          title: 'Gráfica de prueba'
+        },
+        width: 900,
+        height: 500,
+        series: {
           // Gives each series an axis name that matches the Y-axis below.
           0: {axis: 'Temps'},
           
-      },
-      axes: {
+        },
+        axes: {
           // Adds labels to each axis; they don't have to match the axis names.
           y: {
-          	Temps: {label: 'Nº of Played Games'}
+            Temps: {label: 'Nº of Played Games'}
           }
+        }
+      };
+
+
+
+      function drawMaterialChart() {
+        var materialChart = new google.charts.Line(chartDiv);
+        materialChart.draw(data, materialOptions);
+
       }
-  };
 
+      drawMaterialChart();
 
-
-  function drawMaterialChart() {
-  	var materialChart = new google.charts.Line(chartDiv);
-  	materialChart.draw(data, materialOptions);
-  	
     }
-
-  drawMaterialChart();
-
-}
