166fc668fbad2629fabf511b3a6e4e8c53aa4f63
koldointxausti <koldointxaustii@gmail.com>
2020-01-29 10:47:26 +0100

    cambio en API prueba

--- app/Http/Controllers/API/GameController.php
+++ app/Http/Controllers/API/GameController.php
@@ -69,7 +69,8 @@ class GameController extends BaseController
     public function activeGames($game_ids)
     {
         $active_games = [];
-        $game_ids_array = split('_',$game_ids);
+        $game_ids_array = explode('_',$game_ids);
+        return $game_ids_array;
         foreach($game_ids_array as $game_id){
             array_push($active_games,Game::find($game_id)->first());
         }
