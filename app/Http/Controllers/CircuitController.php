ee7b3e2fdad5391890736a6faedc33d92ba1995f
koldointxausti <koldointxaustii@gmail.com>
2020-01-29 09:06:51 +0100

    cambio en API circuit prueba13

--- app/Http/Controllers/CircuitController.php
+++ app/Http/Controllers/CircuitController.php
@@ -181,7 +181,7 @@ class CircuitController extends Controller
         //Esto está programado especificamente para la vista startCaretaker
         //Si se hacen cambios tomar en cuenta que tambien habrá que hacerlos en esa vista
 
-        $circuit = Circuit::find($request->id);
+        $circuit = Circuit::find($id);
         //return $circuit;
         $circuit->join_code = $request->join_code;
         $circuit->save();
