6ba22925fd69763901083d5286c02a4685108714
koldointxausti <koldointxaustii@gmail.com>
2020-01-29 09:56:44 +0100

    cambio en API circuit prueba17

--- app/Http/Controllers/API/CircuitController.php
+++ app/Http/Controllers/API/CircuitController.php
@@ -49,7 +49,7 @@ class CircuitController extends BaseController
         $game_ids = '';
         foreach ($games as $game){
             array_push($aux, $game->user);
-            $game_ids.append($game->id+'_');
+            $game_ids.=$game->id.'_';
         }
         $games = $aux;
         return $this->sendResponse(['games'=>$games, 'game_ids'=>$game_ids], 'Game retrieved succesfully.');
