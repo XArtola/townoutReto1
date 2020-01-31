f6d9b19ece02ec27a04dddc502b894ecdd0ebaf2
XArtola <xartolaza18dw@ikzubirimanteo.com>
2020-01-31 11:16:06 +0100

    Modificación de vista info2

--- resources/views/user/info.blade.php
+++ resources/views/user/info.blade.php
@@ -8,18 +8,18 @@
 	</div>
 	<div class="col-lg-6 col-sm-12">
 		<h1 class="display-4 lead text-center text-break">@lang('user.info')</h1>
-		<p class="p-2 text-justify lead col-10 col-lg-8 col-md-8 mx-auto">@lang('user.whatIsTownout')</p>
-		<p class="pl-2 pb-2 pr-2 pt-0 text-justify lead col-10 col-lg-8 col-md-8 mx-auto">@lang('user.whatIsTownout2')</p>
+		<p class="p-2 text-justify lead col-11 col-lg-8 col-md-8 mx-auto">@lang('user.whatIsTownout')</p>
+		<p class="pl-2 pb-2 pr-2 pt-0 text-justify lead col-11 col-lg-8 col-md-8 mx-auto">@lang('user.whatIsTownout2')</p>
 	</div>
 	<div class="col-12 row">
 		<h1 class="display-4 lead col-12 mt-4 p-2 ml-4 text-center text-break">@lang('user.howToPlay')</h1>
 		<div class="col-lg-8 col-md-8 col-sm-12">
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-8 col-md-8">@lang('user.howToPlayP1')</p>
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-8 col-md-8">@lang('user.howToPlayP2')</p>
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-8 col-md-8">@lang('user.howToPlayP3')</p>
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-8 col-md-8">@lang('user.howToPlayP4')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-8 col-md-8">@lang('user.howToPlayP1')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-8 col-md-8">@lang('user.howToPlayP2')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-8 col-md-8">@lang('user.howToPlayP3')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-8 col-md-8">@lang('user.howToPlayP4')</p>
 			<div class="mx-auto">
-				<table class="table-responsive table-borderless col-10 col-lg-8 col-md-8 mx-auto">
+				<table class="table-responsive table-borderless col-11 col-lg-8 col-md-8 mx-auto">
 					<tr>
 						<td class="font-weight-bold lead p-2">
 							@lang('user.noFails')</td>
@@ -35,7 +35,7 @@
 					</tr>
 				</table>
 			</div>
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-8 col-md-8">@lang('user.howToPlayP5')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-8 col-md-8">@lang('user.howToPlayP5')</p>
 		</div>
 		<div class="col-lg-4 col-sm-12 mx-auto ms-sm-auto my-auto text-center">
 			<img class="col-10 mx-auto img-fluid" src="{{asset('assets/img/mapaJuego.JPG',\App::environment() == 'production')}}"></img>
@@ -45,8 +45,8 @@
 	<div class="col-12 row">
 		<h1 class="display-4 lead col-12 mt-4 p-2 ml-4 text-center text-break">@lang('user.circuitTypes')</h1>
 		<div class="col-lg-8 col-sm-12 mx-auto">
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-12 col-md-12">@lang('user.circuitTypesP1')</p>
-			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-10 col-lg-12 col-md-12">@lang('user.circuitTypesP2')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-12 col-md-12">@lang('user.circuitTypesP1')</p>
+			<p class="pl-2 pt-2 pr-2 text-justify lead mx-auto col-11 col-lg-12 col-md-12">@lang('user.circuitTypesP2')</p>
 		</div>
 	</div>
 	<div class="col-12 row">
@@ -57,11 +57,11 @@
 					<img class="col-10 mx-auto img-fluid pt-sm-2" src="{{asset('assets/img/caretakerFormulario.JPG',\App::environment() == 'production')}}"></img>
 				</div>
 
-				<p class="col-10 col-lg-6 col-lg-6 order-sm-1 order-lg-2 order-sm-1 order-1 pl-2 pt-2 pr-2 text-justify lead mx-auto">@lang('user.guideP1')</p>
+				<p class="col-11 col-lg-6 col-lg-6 order-sm-1 order-lg-2 order-sm-1 order-1 pl-2 pt-2 pr-2 text-justify lead mx-auto">@lang('user.guideP1')</p>
 			</div>
 
 			<div class="row col-12 py-4">
-				<p class="col-10 col-lg-6 col-lg-6 pl-2 pt-2 pr-2 text-justify lead mx-auto my-auto">@lang('user.guideP2')</p>
+				<p class="col-11 col-lg-6 col-lg-6 pl-2 pt-2 pr-2 text-justify lead mx-auto my-auto">@lang('user.guideP2')</p>
 
 				<div class="col-lg-6 col-md-6 col-12 text-center">
 					<img class="col-10 mx-auto img-fluid pt-sm-2" src="{{asset('assets/img/caretakerCircuito.JPG',\App::environment() == 'production')}}"></img>
