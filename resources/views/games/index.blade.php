adf5de5d2b83934cba59839184e18b5c95c26887
koldointxausti <koldointxaustii@gmail.com>
2020-02-05 10:42:38 +0100

    prueba caretaker finish players game

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -318,7 +318,7 @@
                             contentType: "application/json; charset=utf-8",
                             dataType: "json",
                             success: function(data, textStatus, jqXHR) {
-                                alert(data.location['active_circuit'])
+                                alert('success')
                                 if(!data.location.active_circuit){
                                     alert('El organizador del juego ha terminado la partida');
                                 }
