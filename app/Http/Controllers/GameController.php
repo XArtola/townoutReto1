8e00af11dcbbdf4679affa7b52daeff9014b75a3
xabi <xartolaza18dw@ikzubirimanteo.com>
2020-02-05 19:06:48 +0100

    Arreglo join

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -15,6 +15,7 @@ class GameController extends Controller
     // y redirige a la función que inicia la partida
     public function index($id)
     {
+        return 'entra';
         //poner start date
         $game = Game::find($id);
         if ($game->user_id === Auth()->user()->id && $game->start_date == null) {
@@ -77,6 +78,7 @@ class GameController extends Controller
     // Devuelve vista de inserción de código
     public function joinCaretaker()
     {
+        return 'entra';
         return view('games.join');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -95,7 +95,7 @@ Route::group(['middleware' => ['web']], function () {
 
         Route::get('/games/{id}/wait', 'GameController@wait')->name('games.wait');
         Route::get('/games/{id}/startCaretaker', 'GameController@startCaretaker')->name('games.startCaretaker');
-        Route::get('/games/{circuit}/monitor/','GameController@monitor')->name('games.monitor');
+        Route::get('/games/{circuit}/monitor','GameController@monitor')->name('games.monitor');
         Route::put('/games/{circuit}/endCaretaker', 'GameController@endCaretaker')->name('games.endCaretaker');
 
         Route::get('/games/{id}/destroy', 'GameController@destroy')->name('games.destroy');
