7e3f4d06735947d1eedcf04bc426669a3d21f460
koldointxausti <kinchaustide18dw@ikzubirimanteo.com>
2020-02-05 14:04:32 +0100

    games.show arreglo (prueba)

--- app/Http/Controllers/API/LocationController.php
+++ app/Http/Controllers/API/LocationController.php
@@ -149,7 +149,11 @@ class LocationController extends BaseController
 //Devuelve locations de una game especifica
     public function getLocations($id)
     {
-        $locations = Location::where('game_id',$id)->get();
+        $locations_data = Location::where('game_id',$id)->get();
+        $locations = [];
+        foreach($locations_data as $location){
+            array_push($locations, new LocationResource($location));
+        }
         return $this->sendResponse($locations, 'Locations retrieved succesfully.');
     }
 
