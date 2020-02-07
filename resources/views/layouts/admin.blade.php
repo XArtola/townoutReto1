40c7fa6d87bc9d27848d9011fd58414c60e828f0
XArtola <xartolaza18dw@ikzubirimanteo.com>
2020-02-07 08:40:35 +0100

    cambiar ruta de bootstrap.min.css en layouts.admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,11 +13,11 @@
     <title>@yield('title') - Townout</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <!--<link href="{{asset('/assets/lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">-->
 
     <!-- Custom styles for this template -->
-    <link href="{{asset('assets/css/dashboard.css',\App::environment() == 'production')}}" rel="stylesheet">
-    <link href="{{asset('assets/lib/bootstrap/css/bootstrap.min.css',\App::environment() == 'production')}}" rel="stylesheet">
+    <link href="{{asset('/assets/css/dashboard.css',\App::environment() == 'production')}}" rel="stylesheet">
+    <link href="{{asset('/assets/lib/bootstrap/css/bootstrap.min.css',\App::environment() == 'production')}}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
