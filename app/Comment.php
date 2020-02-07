d7ff925736d5e1e1b17348c296e6501039b2a1b0
XArtola <xartolaza18dw@ikzubirimanteo.com>
2020-02-07 13:27:37 +0100

    Arreglo withTrashed

--- app/Comment.php
+++ app/Comment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     public function user (){
-    	return $this->belongsTo('App\User');
+    	return $this->belongsTo('App\User')->withTrashed();
     }
     public function circuit(){
     	return $this->belongsTo('App\Circuit');
